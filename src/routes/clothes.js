f0a3620ea469b5fdfceb76c1d4f0002da7b154e4
Davion Garcia <daviongarcia@gmail.com>
2021-03-05 19:36:07 -0800

    added routes/models

--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -7,10 +7,37 @@ const crud = new Crud();
 
 const router = express.Router();
 
+router.post('/clothes', createRec);
+router.get('/clothes', readAllRec);
+router.get('/clothes/:id', readOneRec);
+router.put('/clothes/:id', updateRec);
+router.delete('/clothes/:id', deleteRec);
 
-//ADD handlers
-router.post('/food', );
-router.get('/food', );
-router.get('/food/:id', );
-router.put('/food/:id', );
-router.delete('/food/:id', );
+function createRec(req, res) {
+    let obj = req.body;
+    let newClothes = crud.create(obj);
+    res.status(201).json(newClothes);
+}
+
+function readAllRec(req, res) {
+    let allRec = crud.get();
+    res.status(200).json(allRec);
+}
+
+function readOneRec(req,res) {
+    let oneRec = crud.get(parseInt(req.params.id));
+    res.status(200).json(oneRec);
+}
+
+function updateRec(req, res) {
+    let obj = req.body;
+    let id = parseInt(req.params.id);
+    let upRec = crud.update(id, obj);
+    res.status(201).json(upRec);
+}
+
+function deleteRec(req, res) {
+    let strikeRec = crud.delete(parseInt(req.params.id));
+}
+
+module.exports = router;
