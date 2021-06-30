1b27799e6305383e2e07594d4ac955db8dbc5fae
Davion <daviongarcia@gmail.com>
2021-06-29 22:57:19 -0700

    mongo stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let server = require('./src/server.js');
 let PORT = process.env.PORT || 3300;
-let URI = process.env.MONGO_URI
+let URI = process.env.MONGO_URI;
 
 const mongoose = require('mongoose');
 const MONGO_URI = URI;
