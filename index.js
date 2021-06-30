af387df4f542d3b93d263ed2320141094335e6df
Davion <daviongarcia@gmail.com>
2021-06-29 22:38:48 -0700

    mongo stuff

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 let server = require('./src/server.js');
 let PORT = process.env.PORT || 3300;
+let URI = procerss.env.MONGO_URI
 
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb+srv://dpg21:niju21@cluster0.dy4zs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_URI = URI;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(MONGO_URI, options)
