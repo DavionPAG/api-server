8fecb36671d85c676b1add67b52ace91e6f299f9
Davion <daviongarcia@gmail.com>
2021-06-29 23:03:13 -0700

    mongo stuff

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const errors = require('./error-handlers/500.js');
 
 const shopRoutes = require('./routes/shop');
 
+app.get('/', (req,res) => res.status(200).json('StoreFront DB'))
+
 app.use(express.json());
 app.use(shopRoutes);
 app.use('*', badRequest); //404 ==> page not found
