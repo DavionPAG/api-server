2b34342adb6d52f27d6dc66ec2aad4ae94d80513
Davion <daviongarcia@gmail.com>
2021-06-29 22:25:54 -0700

    storeFront lab

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let server = require('./src/server.js');
 let PORT = process.env.PORT || 3300;
 
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb://localhost:27017/lab-04-db';
+const MONGO_URI = 'mongodb+srv://dpg21:niju21@cluster0.dy4zs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(MONGO_URI, options)
