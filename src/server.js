a721cbd5ea2f66ebaf8593138519e8c6e9e1181a
Davion <daviongarcia@gmail.com>
2021-07-01 23:53:05 -0700

    cors

--- src/server.js
+++ src/server.js
@@ -5,17 +5,15 @@ express.urlencoded().extened(true)
 const cors = require('cors')
 const app = express();
 
-
-
 const badRequest = require('./error-handlers/404.js');
 const errors = require('./error-handlers/500.js');
 
 app.use(cors)
+app.use(express.json());
 
 const shopRoutes = require('./routes/shop');
 
 app.get('/', (req,res) => res.status(200).json('StoreFront DB'))
-app.use(express.json());
 app.use(shopRoutes);
 app.use('*', badRequest); //404 ==> page not found
 app.use(errors); //500 ==> internal server error
