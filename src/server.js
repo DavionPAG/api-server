48b0e2a976dc7a7dcc855a460a1b419d620cfd89
Davion <daviongarcia@gmail.com>
2021-07-01 23:36:43 -0700

    cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 'use strict';
 
 const express = require('express');
+express.urlencoded()
 const cors = require('cors')
 const app = express();
+
 
 
 const badRequest = require('./error-handlers/404.js');
 const errors = require('./error-handlers/500.js');
 
+app.use(cors)
+
 const shopRoutes = require('./routes/shop');
 
 app.get('/', (req,res) => res.status(200).json('StoreFront DB'))
-
-app.use(cors)
 app.use(express.json());
 app.use(shopRoutes);
 app.use('*', badRequest); //404 ==> page not found
